cacfb548c1550c085a63b3a378a49d6db30b3456
vasanth-877 <156109770+vasanth-877@users.noreply.github.com>
2024-02-06 19:29:01 +0530

    Add files via upload

---
+++ register.js
@@ -0,0 +1,56 @@
+function validate(){
+const form = document.getElementById("form");
+const username = document.getElementById("uname");
+const email = document.getElementById("Email");
+const password = document.getElementById("Pass");
+const phonenumber = document.getElementById("Ph");
+let re1 = /^(?=.*\d)(?=.*[a-z])(?=.*[A-Z])(?=.*[a-zA-Z]).{8,}$/;
+let re2 = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/;
+let re3 = /^[0-9]{10}$/;
+form.addEventListener('submit',(e)=> {
+if(re1.test(password.value))
+{
+    return true;
+    
+}
+else{
+    alert("Invalid Password");
+    password.style.border = "red solid 3px";
+    e.preventDefault();
+    
+}
+
+if(username.value.length >= 15)
+{
+    alert("Username length out of Bounds");
+    username.style.border = "red solid 3px";
+    
+    e.preventDefault();
+}
+else{
+    return true;
+}
+
+if(re2.test(email.value) != true)
+{
+    alert("Ivalid email id");
+    email.style.border = "red solid 3px";
+    e.preventDefault();
+    
+}
+else{
+    return true;
+}
+
+if(re3.test(phonenumber.value) != true)
+{
+    alert("invalid Phone Number");
+    phonenumber.style.border = "red solid 3px";
+    e.preventDefault();
+    
+}
+else
+{
+    return true;
+}
+})}
